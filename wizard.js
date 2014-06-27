a8fc63aa35383974e4d573ff55258b3e7f3a271a
rotherford <luigi@Luis-Mac.local>
2014-06-27 23:39:56 +0200

    okokk

--- wizard.js
+++ wizard.js
@@ -3,10 +3,12 @@ $.fn.wizard = function(options) {
 	var defaults = { 
 	    backButton: 'Back',
 	    nextButton: 'Next',
+	    willDisappear: function() { return true; },
+	    didDisappear: null,
 	  }; 
 
   	var settings = $.extend({}, defaults, options); 
-
+    
     var fields = $(this).find('fieldset');
     var submit = $(this).find('input[type=submit]');
     
@@ -29,8 +31,16 @@ $.fn.wizard = function(options) {
     
     $('.next').click(function() {
      			var parent = $(this).closest('fieldset');
+     			
+     			if (!settings.willDisappear(parent)) {
+     				return;
+     			}
     
      			var next = $(parent).hide().next('fieldset').fadeIn(500);
+     			
+     			if (settings.didDisappear)
+     				settings.didDisappear(parent);
+     			
      			if ($(next).is(':last-of-type')) {
      				$(submit).show();
      			}
@@ -39,12 +49,18 @@ $.fn.wizard = function(options) {
    	$('.back').click(function() {
 	   			var parent = $(this).closest('fieldset');
 	   			
+	   			if (!settings.willDisappear(parent)) {
+	   				return;
+	   			}
+	   			
 	   			$(parent).hide().prev('fieldset').fadeIn(500); 
+	   			
+	   			if (settings.didDisappear)
+	   				settings.didDisappear(parent);
+	   			
 	   			$(submit).hide();
 	   			  		
 	   		});
-	   		
-	
 	   
     return this;
 }
